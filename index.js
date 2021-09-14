5e9e29d0b72613876ca3fd11e34bb190e87de36a
maszhaowei <maszhaowei@gmail.com>
2021-09-15 03:50:51 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as CommonConst from './common/const.js';
 import * as TConst from './tampermonkey/const.js';
-export const Const = Object.assign(CommonConst, TConst);
+export const Const = Object.assign({}, CommonConst, TConst);
 
 import * as commonUtil from './common/util.js';
 import * as tUtil from './tampermonkey/util.js';
@@ -8,4 +8,4 @@ export const util = Object.assign({}, commonUtil, tUtil);
 
 import * as commonUI from './common/ui.js';
 import * as tUI from './tampermonkey/ui.js';
-export const ui = Object.assign(commonUI, tUI);
+export const ui = Object.assign({}, commonUI, tUI);
