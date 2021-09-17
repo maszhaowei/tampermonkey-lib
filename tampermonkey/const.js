b8681daa105af02ce61efaa7466ba35cb1c6c56a
maszhaowei <maszhaowei@gmail.com>
2021-09-17 22:19:15 +0800

    expand const

--- tampermonkey/const.js
+++ tampermonkey/const.js
@@ -2,6 +2,9 @@
 const gmInfo = (typeof GM_info === 'undefined'?{}:GM_info);
 export const scriptInfo = gmInfo && gmInfo.script;
 export const scriptName = scriptInfo && scriptInfo.name;
-export const iframeFullscreenClass = 'player-fullscreen-fix';
-export const eventDelegateClass = 'zw-event-delegate';
-export const topOverlayClass = 'zw-top-overlay';
+export const iframeFullscreenClassName = 'player-fullscreen-fix';
+export const iframeFullscreenSelector = '.' + iframeFullscreenClassName;
+export const eventDelegateClassName = 'zw-event-delegate';
+export const eventDelegateSelector = '.' + eventDelegateClassName;
+export const topOverlayClassName = 'zw-top-overlay';
+export const topOverlaySelector = '.' + topOverlayClassName;
