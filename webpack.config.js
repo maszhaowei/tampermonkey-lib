6b81a6408944a257b6f253e01ab5af64f56ca16c
maszhaowei <maszhaowei@gmail.com>
2021-09-18 14:46:11 +0800

    webpack playerlib

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   entry: {
     commonlib: path.join(__dirname, "common/main.js"),
     tampermonkeylib: path.join(__dirname, "tampermonkey/main.js"),
+    playerlib: path.join(__dirname, 'player/main.js'),
     zwlib: path.join(__dirname, "index.js"),
   },
   output: {
