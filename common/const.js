dd4911936d0fbaafc441298b1fba56d174b7e438
maszhaowei <maszhaowei@gmail.com>
2021-09-18 14:06:45 +0800

    add const

--- common/const.js
+++ common/const.js
@@ -1,1 +1,3 @@
-export const defaultLogGroup = 'Tampermonkey';
+export const DEFAULT_LOG_GROUP = 'Tampermonkey';
+export const VOLUME_STORAGE_KEY = "volume";
+export const PROGRESS_STORAGE_KEY = "progress";
