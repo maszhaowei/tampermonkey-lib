05607b3eacf44b81da9fcfc657f1bb05231a2729
maszhaowei <maszhaowei@gmail.com>
2021-09-19 14:48:10 +0800

    commonlib

--- common/enum.js
+++ common/enum.js
@@ -1,4 +1,4 @@
-import { KeyboardKeyCode, Site, SiteCategory } from "./class";
+import { KeyboardKeyCode } from "./class";
 
 export const KeyboardKeyCodes = {
     Space: new KeyboardKeyCode('Space', ' '),
@@ -31,79 +31,6 @@ export const KeyboardKeyCodes = {
     Numpad8: new KeyboardKeyCode('Numpad8', '8'),
     Numpad9: new KeyboardKeyCode('Numpad9', '9'),
     F8: new KeyboardKeyCode('F8', 'F8')
-};
-/**
- * @enum {SiteCategory}
- */
-export const SiteCategories = {
-    AV: new SiteCategory("AV", /([a-zA-Z]+-\d+)(-(\w+))?/),
-    DB: new SiteCategory("DB"),
-    GAME: new SiteCategory("Game"),
-    IMAGE_HOSTING: new SiteCategory("Image Hosting"),
-    LIVE_STREAMING: new SiteCategory("Live Streaming"),
-    MODDING: new SiteCategory("Modding"),
-    MOVIE: new SiteCategory("Movie"),
-    TV_SERIES: new SiteCategory("TV Series", /([\u4e00-\u9fa5\w]+)第(.+)季.*第(.+)集/),
-    VIDEO_HOSTING: new SiteCategory("Video Hosting"),
-    VIDEO_SHARING: new SiteCategory("Video Sharing")
-};
-/**
- * @enum {string}
- */
-export const SiteIDs = {
-    '7MM': '7MM',
-    AVGLE: 'AVGLE',
-    AVGLE_EMBED: 'AVGLE_EMBED',
-    BILIBILI: 'BILIBILI',
-    BILIBILI_VIDEO: 'BILIBILI_VIDEO',
-    BILIBILI_BANGUMI: 'BILIBILI_BANGUMI',
-    BILIBILI_LIVE: 'BILIBILI_LIVE',
-    BUYCAR5: 'BUYCAR5',
-    DIOUS: 'DIOUS',
-    JABLE: 'JABLE',
-    JAVLIBRARY: 'JAVLIBRARY',
-    JX444662: 'JX444662',
-    MEIJUBS: 'MEIJUBS',
-    MEIJUTTB: 'MEIJUTTB',
-    MINGTIAN6: 'MINGTIAN6',
-    MM9842: 'MM9842',
-    NEXUSMODS: 'NEXUSMODS',
-    QINGBEIBAN: 'QINGBEIBAN',
-    QXWK: 'QXWK',
-    STEAM_COMMUNITY_REVIEW: 'STEAM_COMMUNITY_REVIEW',
-    STEAM_STORE: 'STEAM_STORE',
-    WALLHAVEN: 'WALLHAVEN',
-    WUKONGMEIJU: 'WUKONGMEIJU',
-    YOUTUBE_EMBED: 'YOUTUBE_EMBED'
-};
-/** 
- * @enum {Site} 
- */
-export const Sites = {
-    "7MM": new Site(SiteIDs["7MM"], "https://7mmtv.tv", /^https:\/\/7mmtv\.tv\/.*/, [SiteCategories.AV], ["https://mm9842.com", "https://avgle.com"]),
-    AVGLE: new Site(SiteIDs.AVGLE, "https://avgle.com", /^https:\/\/avgle\.com\/video\/\w+/, [SiteCategories.AV], ["https://7mmtv.tv"]),
-    AVGLE_EMBED: new Site(SiteIDs.AVGLE_EMBED, "https://avgle.com", /^https:\/\/avgle\.com\/embed\/\w+$/, [SiteCategories.AV], ["https://7mmtv.tv"]),
-    BILIBILI: new Site(SiteIDs.BILIBILI, "https://www.bilibili.com", /^https:\/\/www\.bilibili\.com\/.*/, [SiteCategories.VIDEO_SHARING]),
-    BILIBILI_BANGUMI: new Site(SiteIDs.BILIBILI_BANGUMI, "https://www.bilibili.com", /^https:\/\/www\.bilibili\.com\/bangumi\/play\/.+/, [SiteCategories.VIDEO_SHARING]),
-    BILIBILI_LIVE: new Site(SiteIDs.BILIBILI_LIVE, "https://live.bilibili.com", /^https:\/\/live\.bilibili\.com\/.*/, [SiteCategories.LIVE_STREAMING]),
-    BILIBILI_VIDEO: new Site(SiteIDs.BILIBILI_VIDEO, "https://www.bilibili.com", /^https:\/\/www\.bilibili\.com\/video\/.+/, [SiteCategories.VIDEO_SHARING]),
-    BUYCAR5: new Site(SiteIDs.BUYCAR5, "https://vod3.buycar5.cn", /^https:\/\/vod\d+\.buycar5\.cn/, [SiteCategories.VIDEO_HOSTING], ["https://www.meijuttb.com", 'https://www.meijubs.com']),
-    DIOUS: new Site(SiteIDs.DIOUS, "https://v7.dious.cc", /^https:\/\/v7.dious.cc/, [SiteCategories.VIDEO_HOSTING], ["https://www.meijuttb.com", 'https://www.meijubs.com']),
-    JABLE: new Site(SiteIDs.JABLE, "https://jable.tv", /^https:\/\/jable.tv/, [SiteCategories.AV]),
-    JAVLIBRARY: new Site(SiteIDs.JAVLIBRARY, "https://www.javlibrary.com", /^https:\/\/www\.javlibrary\.com\/.*/, [SiteCategories.AV]),
-    JX444662: new Site(SiteIDs.JX444662, "https://jx.444662.cn", /^https:\/\/jx.444662.cn/, [SiteCategories.VIDEO_HOSTING], ["https://www.meijuttb.com", 'https://www.meijubs.com']),
-    MEIJUBS: new Site(SiteIDs.MEIJUBS, "https://www.meijubs.com", undefined, [SiteCategories.TV_SERIES], ["https://vod3.buycar5.cn", "https://jx.444662.cn", "https://vod4.buycar5.cn", "https://v7.dious.cc"]),
-    MEIJUTTB: new Site(SiteIDs.MEIJUTTB, "https://www.meijuttb.com", undefined, [SiteCategories.TV_SERIES], ["https://vod3.buycar5.cn", "https://jx.444662.cn", "https://vod4.buycar5.cn"]),
-    MINGTIAN6: new Site(SiteIDs.MINGTIAN6, "https://www.mingtian6.com", undefined, [SiteCategories.TV_SERIES], ["https://www.qingbeiban.com"]),
-    MM9842: new Site(SiteIDs.MM9842, "https://mm9842.com", /^https:\/\/mm9842.com/, [SiteCategories.AV], ["https://7mmtv.tv"]),
-    NEXUSMODS: new Site(SiteIDs.NEXUSMODS, "https://www.nexusmods.com", undefined, [SiteCategories.MODDING], ["https://www.youtube.com"]),
-    QINGBEIBAN: new Site(SiteIDs.QINGBEIBAN, "https://www.qingbeiban.com", /^https:\/\/www.qingbeiban.com/, [SiteCategories.VIDEO_HOSTING], ["https://www.mingtian6.com"]),
-    QXWK: new Site(SiteIDs.QXWK, "https://code.qxwk.net", /^https:\/\/code.qxwk.net/, [SiteCategories.VIDEO_HOSTING], ["https://m.wukongmeiju.com"]),
-    STEAM_COMMUNITY_REVIEW: new Site(SiteIDs.STEAM_COMMUNITY_REVIEW, "https://steamcommunity.com", /^https:\/\/steamcommunity\.com\/app\/\d+\/reviews\/.+/, [SiteCategories.GAME]),
-    STEAM_STORE: new Site(SiteIDs.STEAM_STORE, "https://store.steampowered.com", /^https:\/\/store\.steampowered\.com\/.*/, [SiteCategories.GAME]),
-    WALLHAVEN: new Site(SiteIDs.WALLHAVEN, "https://wallhaven.cc", /^https:\/\/wallhaven\.cc\/.*/, [SiteCategories.IMAGE_HOSTING]),
-    WUKONGMEIJU: new Site(SiteIDs.WUKONGMEIJU, "https://m.wukongmeiju.com", undefined, [SiteCategories.TV_SERIES], ["https://code.qxwk.net"]),
-    YOUTUBE_EMBED: new Site(SiteIDs.YOUTUBE_EMBED, "https://www.youtube.com", /^https:\/\/www.youtube.com\/embed\/[\w-]+$/, [SiteCategories.VIDEO_SHARING], ['https://www.nexusmods.com'])
 };
 /**
  * @enum {string}
--- common/ui.js
+++ common/ui.js
@@ -1,5 +1,5 @@
 import '../css/tooltip.css';
-import { Sites, SiteCategories, TooltipPosition } from './enum';
+import { TooltipPosition } from './enum';
 import { util } from './util';
 export const ui = {
     /* #region General */
@@ -121,7 +121,7 @@ export const ui = {
         }
         for (let i = 0; i < contexts.length; i++) {
             let context = contexts[i];
-            if(!context) continue;
+            if (!context) continue;
             if (context instanceof Element || context instanceof Document) return context.querySelector(selector);
             else {
                 for (let contextElement of document.querySelectorAll(context)) {
@@ -143,97 +143,7 @@ export const ui = {
      */
     showTooltip: (tooltip, target, options) => { console.debug(tooltip, target, options) },
     /* #endregion */
-    /* #region Business */
-    /**
-     * 
-     * @returns {import('./class').Site} 
-     * @throws 
-     */
-    getCurrentSite() {
-        for (let site in Sites) {
-            if (Sites[site].test()) return Sites[site];
-        }
-        throw "No match for current site";
-    },
-    /**
-     * 
-     * @param {import('./class').Site} [site] 
-     * @returns {import('./class').SiteCategory|undefined} 返回网站类型或undefine(无法确定)
-     * @throws
-     */
-    getCurrentPageCategory(site) {
-        if (!site) site = ui.getCurrentSite();
-        let siteCategories = site.siteCategories;
-        if (!Array.isArray(siteCategories) || siteCategories.length == 0) throw 'Site categories is empty.';
-        else if (siteCategories.length == 1) return siteCategories[0];
-        else if (siteCategories.includes(SiteCategories.TV_SERIES) && SiteCategories.TV_SERIES.titleRegEx.test(document.title)) return siteCategories.TV_SERIES;
-        else if (super.siteCategories && super.siteCategories.includes(SiteCategories.MOVIE)) return SiteCategories.MOVIE;
-    },
-    /* #endregion */
-    /* #region Fullscreen/Webfullscreen */
-    isFullscreen() {
-        return !!(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement);
-    },
-    isFullscreenEnabled() {
-        // return document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled;
-        return util.anyMemberNotEmpty(['fullscreenEnabled',
-            'webkitFullscreenEnabled',
-            'mozFullScreenEnabled',
-            'msFullscreenEnabled'], document)
-    },
-    /**
-     * Returns the Element that is currently being presented in full-screen mode in this document.
-     * @param {boolean} [tryShadowRoot] - Whether or not to get full-screen element from ShadowRoot.
-     * @returns {Element} 
-     */
-    getFullscreenElement(tryShadowRoot) {
-        tryShadowRoot = void 0 === tryShadowRoot ? !1 : tryShadowRoot;
-        /** @type {Element} */
-        var fsEle = util.anyMemberNotEmpty(['fullscreenElement',
-            'webkitFullscreenElement',
-            'mozFullScreenElement',
-            'msFullscreenElement'], document);
-        if (tryShadowRoot) for (; fsEle && fsEle.shadowRoot;) fsEle = fsEle.shadowRoot.fullscreenElement;
-        return fsEle ? fsEle : null;
-    },
-    /**
-     * 
-     * @param {Element} element 
-     * @returns {Promise<undefined|Error>}
-     */
-    requestFullscreen(element = document.documentElement) {
-        let p;
-        if (element.requestFullscreen) p = element.requestFullscreen();
-        else if (element.webkitRequestFullscreen) p = element.webkitRequestFullscreen();
-        else if (element.mozRequestFullScreen) p = element.mozRequestFullScreen();
-        else if (element.msRequestFullscreen) p = element.msRequestFullscreen();
-        else if (element.webkitEnterFullscreen) p = element.webkitEnterFullscreen();
-        else return Promise.reject(Error('Fullscreen API unavailable'));
-        return p instanceof Promise ? p : Promise.resolve();
-    },
-    /**
-     * 
-     * @returns {Promise<undefined|Error>}
-     */
-    exitFullscreen() {
-        let p;
-        if (document.exitFullscreen) p = document.exitFullscreen();
-        else if (document.webkitExitFullscreen) p = document.webkitExitFullscreen();
-        else if (document.mozCancelFullScreen) p = document.mozCancelFullScreen();
-        else if (document.msExitFullscreen) p = document.msExitFullscreen();
-        else return Promise.reject(Error('Exit fullscreen API unavailable'));
-        return p instanceof Promise ? p : Promise.resolve();
-    },
-    /**
-     * 
-     * @param {Element} element 
-     */
-    toggleFullscreen(element = document.documentElement) {
-        if (ui.isFullscreen()) return ui.exitFullscreen(element);
-        else return ui.requestFullscreen(element);
-    }
-    /* #endregion */
-};
+    };
 /**
  * @typedef {object} TooltipOptions
  * @property {string} type 
