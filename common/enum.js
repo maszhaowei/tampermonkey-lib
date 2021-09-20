1181dacc14fd555e1bc4b80d3e4b53ce37f0e57f
maszhaowei <maszhaowei@gmail.com>
2021-09-21 01:18:07 +0800

    enum

--- common/enum.js
+++ common/enum.js
@@ -50,3 +50,30 @@ export const TooltipPosition = {
     RIGHT_BOTTOM: 'right-bottom',
     CENTER_CENTER: 'center-center',
 }
+
+/**
+ * The readiness state of the HTMLMediaElement. 
+ * @enum {number} 
+ */
+export const MediaReadyState = {
+    /** No information is available about the media resource. */
+    HAVE_NOTHING: 0,
+    /** Enough of the media resource has been retrieved that the metadata attributes are initialized. Seeking will no longer raise an exception. */
+    HAVE_METADATA: 1,
+    /** Data is available for the current playback position, but not enough to actually play more than one frame. */
+    HAVE_CURRENT_DATA: 2,
+    /** Data for the current playback position as well as for at least a little bit of time into the future is available (in other words, at least two frames of video, for example). */
+    HAVE_FUTURE_DATA: 3,
+    /** Enough data is available—and the download rate is high enough—that the media can be played through to the end without interruption. */
+    HAVE_ENOUGH_DATA: 4
+};
+/**
+ * @enum {string}
+ */
+export const MediaEvents = {
+    DURATION_CHANGE: 'durationchange',
+    LOADED_METADATA: 'loadedmetadata',
+    PAUSE: 'pause',
+    PLAY: 'play',
+    VOLUME_CHANGE: 'volumechange'
+};
