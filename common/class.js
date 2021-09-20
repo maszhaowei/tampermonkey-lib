ac8d1148e99192a7bccd29f6222c34b656d27b9b
maszhaowei <maszhaowei@gmail.com>
2021-09-20 18:13:54 +0800

    rename

--- common/class.js
+++ common/class.js
@@ -16,7 +16,7 @@ export class KeyboardKeyCode {
         this.key = key;
     }
 }
-class CssCacheItem {
+class CacheItem {
     #callback;
     get callback() { return this.#callback }
     #args;
@@ -28,44 +28,44 @@ class CssCacheItem {
      */
     constructor(callback, args) {
         this.#callback = callback;
-        this.#args = args;
+        this.#args = args || [];
     }
 }
-export class CssCache {
+export class ObjectCacheHelper {
     #site;
-    /** @type {WeakMap<Element, Map<string, CssCacheItem>>} */
-    static #cssMap = new WeakMap();
+    /** @type {WeakMap<Element, Map<string, CacheItem>>} */
+    static #cacheMap = new WeakMap();
     /** @private */
     constructor() { }
     /**
      * 
-     * @param {Element} element 
-     * @param {string} cssName 
+     * @param {object} obj 
+     * @param {string} key 
      * @param {function} callback 
      * @param {any[]} [args] 
      */
-    static saveCss(element, cssName, callback, args) {
-        /** @type {Map<string, CssCacheItem>} */
-        let cssCacheMap;
-        if (CssCache.#cssMap.has(element)) {
-            cssCacheMap = CssCache.#cssMap.get(element);
+    static saveCss(obj, key, callback, args) {
+        /** @type {Map<string, CacheItem>} */
+        let cacheItemMap;
+        if (ObjectCacheHelper.#cacheMap.has(obj)) {
+            cacheItemMap = ObjectCacheHelper.#cacheMap.get(obj);
         }
         else {
-            cssCacheMap = new Map();
-            CssCache.#cssMap.set(element, cssCacheMap);
+            cacheItemMap = new Map();
+            ObjectCacheHelper.#cacheMap.set(obj, cacheItemMap);
         }
-        cssCacheMap.set(cssName, new CssCacheItem(callback, args));
+        cacheItemMap.set(key, new CacheItem(callback, args));
     }
     /**
      * 
-     * @param {Element} element 
-     * @param {string} cssName 
+     * @param {object} obj 
+     * @param {string} key 
      */
-    static restoreCss(element, cssName) {
-        let cssCacheMap = CssCache.#cssMap.get(element);
-        if (cssCacheMap) {
-            let cacheItem = cssCacheMap.get(cssName);
-            if (cacheItem) cacheItem.callback.apply(cacheItem.context || element, cacheItem.args);
+    static restoreCss(obj, key) {
+        let cacheItemMap = ObjectCacheHelper.#cacheMap.get(obj);
+        if (cacheItemMap) {
+            let cacheItem = cacheItemMap.get(key);
+            if (cacheItem) cacheItem.callback.apply(obj, cacheItem.args);
         }
     }
 }
