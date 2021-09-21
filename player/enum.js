2f20f50e27faf092ff17cdde748c8776a83a2aa7
maszhaowei <maszhaowei@gmail.com>
2021-09-21 23:41:39 +0800

    customevent type can't be same with browser event

--- player/enum.js
+++ player/enum.js
@@ -4,9 +4,9 @@
 export const VideoCustomEventTypes = {
     VIDEO_ATTR_INITIALIZED: 'video_attr_initialized',
     VIDEO_READY: 'video_ready',
-    PLAY: 'play',
-    PAUSE: 'pause',
-    VOLUME_CHANGE: 'volume_change',
-    REQUEST_WEBFULLSCREEN: 'request_webfullscreen',
-    EXIT_WEBFULLSCREEN: 'exit_webfullscreen'
+    PLAY: 'video_play',
+    PAUSE: 'video_pause',
+    VOLUME_CHANGE: 'video_volume_change',
+    REQUEST_WEBFULLSCREEN: 'video_request_webfullscreen',
+    EXIT_WEBFULLSCREEN: 'video_exit_webfullscreen'
 }
