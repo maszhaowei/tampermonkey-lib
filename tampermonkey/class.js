a55bd0aa2e30f46e0a60179f594721daa2b2de84
maszhaowei <maszhaowei@gmail.com>
2021-09-21 20:50:17 +0800

    convert

--- tampermonkey/class.js
+++ tampermonkey/class.js
@@ -2,6 +2,12 @@ export class ApplyMethodSignature {
     context;
     fn;
     args;
+    /** @type {(Event)=>void} */
+    #convertedEventHandler;
+    /**
+     * Event handler converted from {@link ApplyMethodSignature} for later removeEventListener.
+     */
+    get convertedEventHandler() { return this.#convertedEventHandler }
     /**
      * 
      * @param {*} context
@@ -12,5 +18,6 @@ export class ApplyMethodSignature {
         this.context = context;
         this.fn = fn;
         this.args = args || [];
+        this.#convertedEventHandler = (e) => this.fn.call(this.context, e);
     }
 }
