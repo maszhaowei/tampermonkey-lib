04e2cb0b6cc966695899430a21e19ed39a737e55
maszhaowei <maszhaowei@gmail.com>
2021-09-24 04:18:47 +0800

    jsdoc

--- tampermonkey/util.js
+++ tampermonkey/util.js
@@ -1,6 +1,15 @@
 import { scriptName } from './const';
 import { EMOJIS } from './enum';
 import { util as cutil } from '../common/util';
+/**
+ * @typedef {object} RandomEmoji
+ * @property {()=>string} RandomEmoji.angry
+ * @property {()=>string} RandomEmoji.happy
+ * @property {()=>string} RandomEmoji.shock
+ * @property {()=>string} RandomEmoji.sad
+ * @property {()=>string} RandomEmoji.helpless
+ * @property {()=>string} RandomEmoji.custom
+ */
 export const util = {
     /**
      * Output message to web console in gourp {@link scriptName}.
@@ -19,6 +28,7 @@ export const util = {
     printSendMessage: function (targetOrigin, message) {
         util.debug(`<<< To: ${targetOrigin}, From: ${window.location.origin}, Message:`, message);
     },
+    /** @type RandomEmoji */
     randomEmoji: {}
 };
 for (let i in EMOJIS) {
