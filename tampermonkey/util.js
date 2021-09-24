0ad496d0cae75e4df4053cbe1e37c5912ba07a9d
maszhaowei <maszhaowei@gmail.com>
2021-09-25 02:30:04 +0800

    post support blob

--- tampermonkey/util.js
+++ tampermonkey/util.js
@@ -49,12 +49,13 @@ export const util = {
      */
     gmPost: function (url, headers, data, responseType) {
         headers = headers || { "Content-Type": "application/x-www-form-urlencoded" };
+        if (cutil.isObject(data)) data = JSON.stringify(data);
         return new Promise((resolve, reject) => {
             GM_xmlhttpRequest({
                 method: "POST", url, headers, data,
                 responseType: responseType || 'json',
                 onload: (res) => {
-                    resolve(res.response || res.responseText);
+                    responseType === 'blob' ? resolve(res) : resolve(res.response || res.responseText);
                 },
                 onerror: (err) => {
                     reject(err);
