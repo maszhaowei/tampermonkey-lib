28955f4a8600b09efe1e5b1907a7525df5237408
maszhaowei <maszhaowei@gmail.com>
2021-11-02 17:33:31 +0800

    GMStorageHelper

--- tampermonkey/utils.js
+++ tampermonkey/utils.js
@@ -1,4 +1,5 @@
 import { ApplyMethodSignature, Tuple, LooseMap } from "../common/class";
+import { util } from "./util";
 export class CssCacheHelper {
     static #cacheMap = new LooseMap();
     /** 
@@ -33,5 +34,46 @@ export class CssCacheHelper {
             return true;
         }
         return false;
+    }
+}
+
+export class GMStorageHelper {
+    /**
+     * 
+     * @param {string} name 
+     * @param {*} [defaultValue] 
+     * @returns 
+     */
+    static getValue(name, defaultValue) {
+        let valueObj = GM_getValue(name, defaultValue);
+        if (valueObj && valueObj.value && valueObj.expireTime) return valueObj.value;
+        else return valueObj;
+    }
+    /**
+     * 
+     * @param {string} name 
+     * @param {*} value 
+     * @param {number} [expireDays] Expiration timeout in days. Default to 30. Set to 0 to never expire.
+     */
+    static setValue(name, value, expireDays = 30) {
+        let expireTime = expireDays > 0 ? (Date.now() + expireDays * 24 * 3600 * 1000) : 0;
+        GM_setValue(name, { value: value, expireTime: expireTime });
+    }
+    /**
+     * Dependency: GM_listValues, GM_deleteValue.
+     */
+    static clearExpiredValues() {
+        let names = GM_listValues();
+        let count = 0;
+        names.forEach((name) => {
+            let valueObj = GM_getValue(name);
+            let expireTime = valueObj.expireTime;
+            if (!expireTime) return;
+            if (Date.now() > expireTime) {
+                GM_deleteValue(name);
+                count++;
+            }
+        });
+        util.debug(`Deleted ${count} records from storage.`)
     }
 }
