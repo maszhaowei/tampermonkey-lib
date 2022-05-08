59e1aa100defcc13afcbf5df2e5ec7de1390f6c6
maszhaowei <maszhaowei@gmail.com>
2022-05-09 02:52:00 +0800

    migrate to ts

--- packages/tampermonkey/enum.ts
+++ packages/tampermonkey/enum.ts
@@ -1,43 +1,6 @@
 /**
  * @enum {string}
  */
-export const MessageTypes = {
-    GENERAL: 'general',
-    SITE_INFO: 'site_info',
-    VIDEO_ATTR_INITIALIZED: 'video_attr_initialized',
-    VIDEO_READY: 'video_ready',
-    SWITCH_PLAYING_VIDEO: 'switch_playing_video',
-    REMOVE_IFRAME: 'remove_iframe',
-    READY_FOR_MESSAGE: 'ready_for_message',
-    REQUEST_WEBFULLSCREEN: 'request_webfullscreen',
-    EXIT_WEBFULLSCREEN: 'exit_webfullscreen',
-    KEYDOWN: 'keydown',
-    KEYUP: 'keyup',
-    VIEWPORT_DIMENSION: 'viewport_dimension',
-    /**
-     * 
-     * @param {string} value 
-     * @returns 
-     */
-    hasValue: function (value) {
-        if (value === MessageTypes.hasValue) return false;
-        for (let i in MessageTypes) {
-            if (this[i] === value) return true;
-        }
-        return false;
-    }
-};
-/**
- * @enum {string}
- */
-export const PlayerMessageTypes = {
-    PREVIOUS_EPISODE: 'previous_episode',
-    NEXT_EPISODE: 'next_episode',
-    EXIT_PLAYER: 'exit_player'
-};
-/**
- * @enum {string}
- */
 export const EMOJIS = {
     angry: [
         ',,Ծ‸Ծ,,',
